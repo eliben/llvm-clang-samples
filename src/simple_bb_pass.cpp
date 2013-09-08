decf3529d856a6f87a0a26e5a4fa6ea82ba61a31
Eli Bendersky <eliben@google.com>
2013-09-08 08:20:01 -0700

    Clean up needless includes

--- src/simple_bb_pass.cpp
+++ src/simple_bb_pass.cpp
@@ -15,13 +15,11 @@
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/Module.h"
-#include "llvm/IR/Operator.h"
 #include "llvm/IRReader/IRReader.h"
 #include "llvm/Pass.h"
 #include "llvm/PassManager.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Support/SourceMgr.h"
-#include <cstdlib>
 
 using namespace llvm;
 
