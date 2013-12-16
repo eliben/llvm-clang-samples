8fc674de05ddd8e6d6de3ddccf91c0977696dbf6
Eli Bendersky <eliben@gmail.com>
2013-12-16 15:03:38 -0800

    gah, forgot to add the code

---
+++ using_clang_toolchain/exceptions1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <exception>
+#include <stdexcept>
+
+void foo(int i) {
+  if (i > 2) {
+    throw std::runtime_error("Expected i <= 2");
+  }
+}
+
+int main(int argc, char** argv) {
+  try {
+    foo(argc);
+  } catch (std::exception& e) {
+    std::cerr << "Got exception: " << e.what() << std::endl;
+  }
+  
+  return 0;
+}
+
