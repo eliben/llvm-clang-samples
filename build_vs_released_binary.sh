758523727739d03a682a26ef7d6d606565074fd3
Eli Bendersky <eliben@google.com>
2016-03-07 13:52:39 -0800

    Adding script that builds vs. released binary drop

---
+++ build_vs_released_binary.sh
@@ -0,0 +1,24 @@
+#-------------------------------------------------------------------------------
+# Shows how to build the samples vs. a released binary download of LLVM & Clang.
+#
+# Assumes the binary release was downloaded from http://llvm.org/releases/
+# and untarred in some directory. The actual script code here uses a location
+# I use on my machine, but that can be replaced by anything you fancy in the
+# BINARY_DIR_PATH variable.
+#
+# Eli Bendersky (eliben@gmail.com)
+# This code is in the public domain
+#-------------------------------------------------------------------------------
+
+#!/bin/bash
+
+set -eu
+set -x
+
+BINARY_DIR_PATH=$HOME/llvm/llvm3.7-binaries
+
+make -j4 \
+  CXX=$BINARY_DIR_PATH/bin/clang++ \
+  LLVM_SRC_PATH=$BINARY_DIR_PATH \
+  LLVM_BUILD_PATH=$BINARY_DIR_PATH/bin \
+  LLVM_BIN_PATH=$BINARY_DIR_PATH/bin
